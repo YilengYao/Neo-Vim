e93a41c8adf764c10449caa9ec2dc9d358fa5c74
yilengyao <31053252+yilengyao@users.noreply.github.com>
2024-10-28 09:15:35 -0700

    format on save

--- mac-neovim-config/nvim/init.lua
+++ mac-neovim-config/nvim/init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- format on save
+vim.cmd([[
+  augroup FormatOnSave
+    autocmd!
+    autocmd BufWritePre * lua vim.lsp.buf.format()
+  augroup END
+]])
